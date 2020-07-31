4e4ae98fa431d13d7e427b5b571ef3f22d58f04e
Ao.Nagasawa <aho_ao1004@yahoo.co.jp>
2020-07-31 10:46:26 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by Ao Nagasawa on 2020/07/31.
+//  Copyright © 2020 Ao.Nagasawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
